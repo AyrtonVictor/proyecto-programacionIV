a790c054a368be5f084dde2b436380e6f41837d7
Ayrton Victor <ayrtonvictor0@gmail.com>
2026-08-12 15:45:55 -0300

    Comit 9 - Agrego CSS y Editar y Eliminar

--- js/alumnos.js
+++ js/alumnos.js
@@ -1,4 +1,7 @@
 const formulario = document.querySelector("#formAlumno")
+const mensaje = document.querySelector("#mensaje")
+const listaAlumnos = document.querySelector("#listaAlumnos")
+
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -17,6 +20,8 @@ formulario.addEventListener("submit", function (event) {
     const alumnos = obtenerAlumnos()
     alumnos.push(alumno)
     localStorage.setItem("alumnos", JSON.stringify(alumnos))
+    mostrarMensaje("Alumno guardado correctamente.")
+
     mostrarAlumnos(alumnos)
     formulario.reset()
 });
@@ -29,16 +34,67 @@ function obtenerAlumnos() {
     return []
 }
 
-const listaAlumnos = document.querySelector("#listaAlumnos")
+function mostrarMensaje(texto) {///Esto es para que le texto desaparezca despues de 3 segundos
+    mensaje.textContent = texto;
+    setTimeout(() => {
+        mensaje.textContent = " ";
+    }, 3000);
+}
 
+
+//Esto va agregando a la lista que se mostrara en el index
 function mostrarAlumnos(alumnos) {
     listaAlumnos.innerHTML = ""
     for (const alumno of alumnos) {
         listaAlumnos.innerHTML += `
-        <li>
-            ${alumno.nombre} - 
-            ${alumno.carrera} -
-            ${alumno.correo}
-        </li>`
+        <tr>
+            <td>${alumno.id}</td>
+            <td>${alumno.nombre}</td>
+            <td>${alumno.carrera}</td>
+            <td>${alumno.correo}</td>
+            <td>
+                <button class="btn-editar" data-id="${alumno.id}" >Editar<button>
+                <button class="btn-eliminar" data-id="${alumno.id}">Eliminar<button>
+            </td>
+        </tr>
+        `
     }
+}
+
+// <li>
+//     ${alumno.nombre} - 
+//     ${alumno.carrera} -
+//     ${alumno.correo}
+// </li>
+
+
+
+//Esta funcion elimina al alumno en la lista.
+function eliminarAlumno(id) {
+    const alumnos = obtenerAlumnos()
+    const alumnosActualizados = alumnos.filter(
+        alumno => alumno.id != id //genera el array sin el alumno que queremos eliminar
+    );
+
+    localStorage.setItem("alumnos", JSON.stringify(alumnosActualizados))
+    mostrarAlumnos(alumnosActualizados)
+    mostrarMensaje("Alumno eliminado correctamente.")
+}
+
+
+
+listaAlumnos.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-eliminar")) {
+        const id = Number(e.target.dataset.id)
+        eliminarAlumno(id)
+    }
+})
+
+function editarAlumno(id) {
+    const alumnos = obtenerAlumnos()
+    const alumno = alumnos.find(alumno => alumno.id === id)
+    document.querySelector("#nombre").value = alumno.nombre;
+    document.querySelector("#carrera").value = alumno.carrera;
+    document.querySelector("#correo").value = alumno.correo;
+    alumnoEditandoId = id;
 }
